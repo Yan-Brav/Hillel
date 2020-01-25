aa2c10f36a0207f57d66b233db2a081ff376aec9
Yan-Brav <yan.braverman37@gmail.com>
2020-01-25 12:47:13 +0200

    2020_01_25_ added scriptL2_1.js

--- Lesson_2/scriptL2_1.js
+++ Lesson_2/scriptL2_1.js
@@ -1,2 +1,30 @@
-console.log(new Number(5) == 5);
-console.log();
+//Object isn't equal number
+console.log(new Number(5) === 5);
+//Objects aren't equal each other
+console.log("-----------------------");
+console.log(new Number(5) == new Number(5));
+console.log("--------------------------");
+console.log({} === {});
+// number and string have equal values
+console.log("--------------------------");
+console.log(5 == "5");
+// number and string aren't equal each other
+console.log("--------------------------");
+console.log(5 === "5");
+// NaN isn't equal another NaN
+console.log("--------------------------");
+console.log(NaN == NaN);
+// NaN isn't equal another NaN
+console.log("--------------------------");
+console.log(Infinity === Infinity);
+// NaN isn't equal another NaN
+console.log("--------------------------");
+console.log(0 == false);
+console.log("" == false);
+console.log([] == false);
+//Why???
+console.log(null == false);
+console.log(undefined == false);
+var x = NaN;
+console.log(x !== x);
+console.log(x === x);
