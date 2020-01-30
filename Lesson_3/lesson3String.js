91400b785987f282d69479127e42b57fe187dced
Yan-Brav <yan.braverman37@gmail.com>
2020-01-30 12:15:00 +0200

    2020_01_30_The home task was improved

--- Lesson_3/lesson3String.js
+++ Lesson_3/lesson3String.js
@@ -2,8 +2,40 @@
 
 //DOM (Document Object Model) it is API which interacts with HTML or XML documents. It is model which downloaded in
 //browser. DOM presents documents as tree of nodes (element, text or comment).
-//BOM (Browser Object Model) it is additional browser's objects which can work with anything except document
-//such as alert, confirm, prompt, location, navigator and many others.
+/*BOM provides access to the browser window and allows you to manipulate it and its elements.
+    BOM objects provide access to browser functionality regardless of the content of the web page.
+    Using the Browser Object Model (BOM), you can control browser behavior from JavaScript.
+    BOM includes several objects.
+    ----------------Window object------------------------
+    In browsers the Window object plays the role of a global object. It cannot be accessed directly, however, it has
+    a window property that refers to the object itself, which can be used instead of the this keyword
+    to refer to the global object.
+    All variables and functions declared globally become its properties and methods.
+    Methods: alert, confirm, close, focus, open, print, prompt, stop and many others.
+    Properties: document, frames, history, length, location, navigator, parent and many others.
+    --------------Object Navigator---------------------------
+    The Navigator object is a property of the Window object and is available in all browsers.
+    Properties: appName, appVersion, geolocation, platform and others.
+    --------------Screen Object-------------------------------
+    The Screen object (which is also a property of the Window object) is one of the few JavaScript objects that are hardly used in the code.
+    It simply provides information about the graphical parameters of the client system outside the browser window, such as width and height in pixels.
+    The availability of certain properties of the Screen object depends on the browser.
+    Properties: availHeight, availWidth, colorDepth and others.
+    -------------History object-----------------------------
+    The History object provides a navigation log for the entire time you work with a particular window.
+    Length property
+    Methods: back, forward and go.
+    ------------Location object--------------------------------
+    The Location object contains information about the URL of the current document with which it is associated.
+    To change or get the current URL, it is not necessary to use the methods or properties of the object, you can also do this by
+    contacting the location property directly:
+    For example:
+    var someURL = window.location;
+    window.location = 'httr: //www.puzzleweb.ru';
+
+    Methods: assign, reload, replace.
+    Properties: hash, host, hostname, href, origin, pathname, port, protocol and search*/
+
 
 //Task#2 ---------------------------------------------------
 
