a3b1a9044b8d1efea9a0f4fc5eb0827d7e89c6a2
Yan-Brav <yan.braverman37@gmail.com>
2020-02-06 10:33:09 +0200

    2020_02_06_the home task for lesson 5 has been corrected

--- Lesson_5/lesson5.js
+++ Lesson_5/lesson5.js
@@ -1,21 +1,10 @@
-var regLogin = /^[A-Z].*\D\d{2}$/g;
-var regPassword = /^[a-zа-я]{1,5}$/g;
+var regLogin = /^[A-Z].*\D\d{2}$/;
+var regPassword = /^[a-zа-я]{1,5}$/;
 var login = prompt("Enter your login");
 var password = prompt("Enter your password");
 
-if (!regLogin.test(login)){
-    alert("Be careful. Try to enter your login again");
-}
-!regLogin.test(login);
 console.log(regLogin.test(login));
-if (!regPassword.test(password)){
-    alert("Be careful. Try to enter your password again");
-}
-!regPassword.test(password);
 console.log(regPassword.test(password));
-
-alert("Hello " + login);
-
 
 
 
