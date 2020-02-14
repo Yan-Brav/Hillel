f14136487522b56338a5c51312b4bd2bebe44577
Yan-Brav <yan.braverman37@gmail.com>
2020-02-14 19:50:21 +0200

    2020_02_14 home task for lesson 7 has been done

--- Lesson_8/compareArrays.js
+++ Lesson_8/compareArrays.js
@@ -7,9 +7,9 @@ function f1() {
     return "Donic";
 }
 const firstArray = [1, true, 25, "str1", "str123", 35, 8,
-    Infinity, arr, [2, 4, 6], false, null, 124, "Stiga", obj, f1()];
+    Infinity, arr, false, null, 124, "Stiga", obj, f1()];
 const secondArray = [8, 2, true, 25, "Stiga", null, Infinity, arr,
-    "str123", [2, 4, 6], false, "str123", 35, obj, f1(), 50];
+    "str123", false, "str123", 35, obj, f1(), 50];
 let resultArray = [];
 firstArray.forEach(function (element) {
     secondArray.filter(function (item){
