65992e8c79e10401f8a498d3932b32bf40f59409
Yan-Brav <yan.braverman37@gmail.com>
2020-05-05 16:33:15 +0300

    2020_05_05 result showing has been improved

--- Lesson_25/public/js/users.js
+++ Lesson_25/public/js/users.js
@@ -1,15 +1,24 @@
 'use strict';
 window.addEventListener('load', function () {
     const button = document.querySelector('button');
-    const p = document.querySelector('p');
+    // const p = document.querySelector('p');
+    const div = document.querySelector('div');
+    const usersList = document.createElement('ol');
     const requestURL = 'http://localhost:3333/users';
+
+    div.appendChild(usersList);
 
     async function getUsers() {
         try {
             const response = await fetch(requestURL);
-            p.innerText = await response.json();
-        }catch (e) {
-            console.error(e);
+            const data = await response.json();
+            data.forEach(user => {
+                const item = document.createElement('li');
+                item.innerText = user.name + ' ' + user.phone;
+                usersList.appendChild(item);
+            })
+        }catch (err) {
+            console.error('Something wrong');
         }
     }
     button.addEventListener('click', getUsers);
